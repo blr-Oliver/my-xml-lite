73b5154bea98cba709e9fab33cfb98cc12f23a9e
blr-Oliver <vasily.liaskovsky@gmail.com>
2024-03-27 09:54:21 +0300

    reconsume capability added to character source

--- src/common/stream-source.ts
+++ src/common/stream-source.ts
@@ -7,6 +7,11 @@ export interface CharacterSource {
    * @return current (last returned) valid Unicode code point (including characters outside BMP) or -1 if the source is exhausted or -2 if no code point was drained yet
    */
   get(): number;
+  /**
+   * Reverts the source one code point back.
+   * @return previous Unicode code point or -2 if no code point was drained yet
+   */
+  reconsume(): number;
 }
 
 export interface Resettable {
@@ -28,7 +33,7 @@ export interface StringSource extends CharacterSource {
 
 export class BufferedStringSource implements StringSource, Resettable {
   private readonly buffer: Uint32Array;
-  private position: number = -1;
+  private position: number = 0;
 
   constructor(private readonly source: CharacterSource,
               bufferSize: number = 1 << 14 /* 16K */) {
@@ -36,11 +41,15 @@ export class BufferedStringSource implements StringSource, Resettable {
   }
 
   next(): number {
-    return this.buffer[++this.position] = this.source.next();
+    return this.buffer[this.position++] = this.source.next();
   }
 
   get(): number {
     return this.source.get();
+  }
+
+  reconsume(): number {
+    return this.buffer[--this.position] = this.source.reconsume();
   }
 
   start(): number {
@@ -84,10 +93,16 @@ export abstract class ArrayCharacterSource<T extends ArrayLike<number>> implemen
   }
 
   protected abstract consumeNext(): number;
+  protected abstract consumePrevious(): number;
 
   next(): number {
     if (this.position >= this.limit) return this.code = -1;
     return this.code = this.consumeNext();
+  }
+
+  reconsume(): number {
+    if (this.position <= this.offset) return -2;
+    return this.code = this.consumePrevious();
   }
 
   get(): number {
@@ -118,6 +133,9 @@ export class DirectCharacterSource extends ArrayCharacterSource<Uint16Array> imp
   protected consumeNext(): number {
     return this.data[this.position++];
   }
+  protected consumePrevious(): number {
+    return this.data[--this.position];
+  }
   start(): number {
     this.mark = this.position;
     return this.get();
@@ -132,10 +150,16 @@ export class UTF16NonValidatingCharacterSource extends ArrayCharacterSource<Uint
     super(data, limit, offset);
   }
 
-  consumeNext(): number {
+  protected consumeNext(): number {
     const word1 = this.data[this.position++];
     if (word1 < 0xD800 || word1 >= 0xE000) return word1;
     const word2 = this.data[this.position++];
+    return (0x10000 + (((word1 & 0x3FF) << 10) | (word2 & 0x3FF)));
+  }
+  protected consumePrevious(): number {
+    const word2 = this.data[--this.position];
+    if (word2 < 0xD800 || word2 >= 0xE000) return word2;
+    const word1 = this.data[--this.position];
     return (0x10000 + (((word1 & 0x3FF) << 10) | (word2 & 0x3FF)));
   }
 }
@@ -145,7 +169,7 @@ export class UTF16ValidatingCharacterSource extends ArrayCharacterSource<Uint16A
     super(data, limit, offset);
   }
 
-  consumeNext(): number {
+  protected consumeNext(): number {
     const word1 = this.data[this.position++];
     if (word1 < 0xD800 || word1 >= 0xE000) return word1;
     if (word1 >= 0xDC00)
@@ -157,6 +181,23 @@ export class UTF16ValidatingCharacterSource extends ArrayCharacterSource<Uint16A
       throw new Error(`Expected trailing surrogate, found 0x${word1.toString(16).toUpperCase()} at ${this.position - 1}`);
     return (0x10000 + (((word1 & 0x3FF) << 10) | (word2 & 0x3FF)));
   }
+  protected consumePrevious(): number {
+    const word2 = this.data[--this.position];
+    if (word2 < 0xD800 || word2 >= 0xE000) return word2;
+    if (word2 < 0xDC00)
+      throw new Error(`Expected trailing surrogate, found leading surrogate 0x${word2.toString(16).toUpperCase()} at ${this.position++}`);
+    if (this.position <= this.offset) {
+      ++this.position;
+      throw new Error(`Expected leading surrogate, found start of input`);
+    }
+    const word1 = this.data[--this.position];
+    if (word1 < 0xD800 || word1 >= 0xDC00) {
+      const message = `Expected leading surrogate, found 0x${word1.toString(16).toUpperCase()} at ${this.position}`;
+      this.position += 2;
+      throw new Error(message);
+    }
+    return (0x10000 + (((word1 & 0x3FF) << 10) | (word2 & 0x3FF)));
+  }
 }
 
 export class UTF8NonValidatingCharacterSource extends ArrayCharacterSource<Uint8Array> {
@@ -164,11 +205,22 @@ export class UTF8NonValidatingCharacterSource extends ArrayCharacterSource<Uint8
     super(data, limit, offset);
   }
 
-  consumeNext(): number {
+  protected consumeNext(): number {
     const byte1 = this.data[this.position++];
     const leadingOnes = Math.clz32(~byte1 & 0xFF) - 24;
     if (leadingOnes === 0) return byte1;
     return this.withTails(byte1, leadingOnes - 1);
+  }
+  protected consumePrevious(): number {
+    let byte = this.data[--this.position];
+    if (byte < 0x80) return byte;
+    let value = 0, tailLength = 0;
+    while (~(byte | 0xBF)) {
+      value |= ((byte & 0x3F) << (6 * tailLength++));
+      byte = this.data[--this.position];
+    }
+    value |= (byte & (0x3F >> tailLength)) << (6 * tailLength);
+    return value;
   }
 
   private withTails(byte1: number, count: number): number {
