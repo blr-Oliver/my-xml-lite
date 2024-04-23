5318ff1cc15ac4d23e62ca97cc5e3bd7c894a145
blr-Oliver <vasily.liaskovsky@gmail.com>
2024-04-23 17:36:40 +0300

    text-specific code moved Base -> Text

--- src/impl/fragmented/BaseTokenizer.ts
+++ src/impl/fragmented/BaseTokenizer.ts
@@ -1,4 +1,4 @@
-import {EOC, FF, GT, isAsciiLowerAlpha, isAsciiUpperAlpha, LF, SOLIDUS, SPACE, TAB} from '../../common/code-points';
+import {EOC} from '../../common/code-points';
 import {ParserEnvironment} from '../../decl/ParserEnvironment';
 import {Attribute, TagToken} from '../tokens';
 import {State} from './states';
@@ -74,53 +74,9 @@ export abstract class BaseTokenizer {
     }
   }
 
-  private startTagIfMatches(expectedTag: string, emitIfMatched: boolean = false, fromPosition: number): boolean {
-    const buffer = this.env.buffer;
-    const name = buffer.getString(fromPosition);
-    const matches = name === expectedTag;
-    if (matches) {
-      this.emitAccumulatedCharacters();
-      this.startNewTag(name);
-      buffer.clear();
-      if (emitIfMatched)
-        this.emitCurrentTag();
-      return true;
-    }
-    return false;
-  }
-
   protected callState(state: State, code: number): State {
     this.state = state;
     // @ts-ignore
     return this[state](code);
-  }
-
-  protected expectAsciiTag(code: number, tag: string, failedState: State): State {
-    // TODO instead of tag parameter actually use last open tag
-    const buffer = this.env.buffer;
-    const mark = buffer.position;
-    while (true) {
-      switch (code) {
-        case TAB:
-        case LF:
-        case FF:
-        case SPACE:
-          if (this.startTagIfMatches(tag, false, mark)) return 'beforeAttributeName';
-          else return this.callState(failedState, code);
-        case SOLIDUS:
-          if (this.startTagIfMatches(tag, false, mark)) return 'selfClosingStartTag';
-          else return this.callState(failedState, code);
-        case GT:
-          if (this.startTagIfMatches(tag, false, mark)) return 'data';
-          else return this.callState(failedState, code);
-        default:
-          if (isAsciiUpperAlpha(code)) code += 0x20;
-          if (isAsciiLowerAlpha(code)) {
-            buffer.append(code);
-            code = this.nextCode();
-          } else
-            return this.callState(failedState, code);
-      }
-    }
   }
 }
--- src/impl/fragmented/TextTokenizer.ts
+++ src/impl/fragmented/TextTokenizer.ts
@@ -1,4 +1,19 @@
-import {AMPERSAND, EOF, isAsciiAlpha, LT, NUL, REPLACEMENT_CHAR, SOLIDUS} from '../../common/code-points';
+import {
+  AMPERSAND,
+  EOF,
+  FF,
+  GT,
+  isAsciiAlpha,
+  isAsciiLowerAlpha,
+  isAsciiUpperAlpha,
+  LF,
+  LT,
+  NUL,
+  REPLACEMENT_CHAR,
+  SOLIDUS,
+  SPACE,
+  TAB
+} from '../../common/code-points';
 import {EOF_TOKEN} from '../tokens';
 import {BaseTokenizer} from './BaseTokenizer';
 import {State} from './states';
@@ -80,5 +95,49 @@ export abstract class TextTokenizer extends BaseTokenizer {
       buffer.append(SOLIDUS);
       return this.callState(dataState, code);
     }
+  }
+
+  protected expectAsciiTag(code: number, tag: string, failedState: State): State {
+    // TODO instead of tag parameter actually use last open tag
+    const buffer = this.env.buffer;
+    const mark = buffer.position;
+    while (true) {
+      switch (code) {
+        case TAB:
+        case LF:
+        case FF:
+        case SPACE:
+          if (this.startTagIfMatches(tag, false, mark)) return 'beforeAttributeName';
+          else return this.callState(failedState, code);
+        case SOLIDUS:
+          if (this.startTagIfMatches(tag, false, mark)) return 'selfClosingStartTag';
+          else return this.callState(failedState, code);
+        case GT:
+          if (this.startTagIfMatches(tag, false, mark)) return 'data';
+          else return this.callState(failedState, code);
+        default:
+          if (isAsciiUpperAlpha(code)) code += 0x20;
+          if (isAsciiLowerAlpha(code)) {
+            buffer.append(code);
+            code = this.nextCode();
+          } else
+            return this.callState(failedState, code);
+      }
+    }
+  }
+
+  private startTagIfMatches(expectedTag: string, emitIfMatched: boolean = false, fromPosition: number): boolean {
+    const buffer = this.env.buffer;
+    const name = buffer.getString(fromPosition);
+    const matches = name === expectedTag;
+    if (matches) {
+      this.emitAccumulatedCharacters();
+      this.startNewTag(name);
+      buffer.clear();
+      if (emitIfMatched)
+        this.emitCurrentTag();
+      return true;
+    }
+    return false;
   }
 }
