8472fda080d38e8575ebef9ee5fb37f98ee41fea
blr-Oliver <vasily.liaskovsky@gmail.com>
2024-05-26 02:47:38 +0300

    break apart composer implementation

---
+++ src/impl/composer/BaseComposer.ts
@@ -0,0 +1,197 @@
+import {TokenSink} from '../../decl/ParserEnvironment';
+import {Node, NodeType} from '../../decl/xml-lite-decl';
+import {StaticDataNode} from '../nodes/StaticDataNode';
+import {StaticDocument} from '../nodes/StaticDocument';
+import {StaticDocumentType} from '../nodes/StaticDocumentType';
+import {StaticElement} from '../nodes/StaticElement';
+import {StaticParentNode} from '../nodes/StaticParentNode';
+import {StateBasedTokenizer} from '../StateBasedTokenizer';
+import {State} from '../states';
+import {DoctypeToken, TagToken, TextToken, Token} from '../tokens';
+import {InsertionMode} from './insertion-mode';
+
+/*
+void elements
+area, base, br, col, embed, hr, img, input, link, meta, source, track, wbr
+*/
+const TokenTypeMapping = {
+  'doctype': NodeType.DOCUMENT_TYPE_NODE,
+  'startTag': NodeType.ELEMENT_NODE,
+  'endTag': NodeType.ELEMENT_NODE,
+  'comment': NodeType.COMMENT_NODE,
+  'characters': NodeType.TEXT_NODE,
+  'cdata': NodeType.CDATA_SECTION_NODE
+};
+
+const IMPLICITLY_CLOSABLE = {
+  'dd': true,
+  'dt': true,
+  'li': true,
+  'optgroup': true,
+  'option': true,
+  'p': true,
+  'rb': true,
+  'rp': true,
+  'rt': true,
+  'rtc': true
+};
+
+const IMPLICITLY_THOROUGHLY_CLOSABLE = {
+  ...IMPLICITLY_CLOSABLE,
+  'caption': true,
+  'colgroup': true,
+  'tbody': true,
+  'td': true,
+  'tfoot': true,
+  'th': true,
+  'thead': true,
+  'tr': true
+};
+
+
+export class BaseComposer implements TokenSink {
+
+  tokenizer!: StateBasedTokenizer;
+  insertionMode: InsertionMode = 'initial';
+  originalInsertionMode!: InsertionMode;
+
+  document!: StaticDocument;
+
+  openElements: StaticElement[] = [];
+  openCounts: { [tagName: string]: number } = {};
+  scopeCounter: number = 0;
+  listScopeCounter: number = 0;
+  tableScopeCounter: number = 0;
+
+  current!: StaticParentNode;
+  headElement!: StaticElement;
+  get currentChildNodes(): Node[] {
+    return this.current.childNodes;
+  }
+  get currentChildElements(): StaticElement[] {
+    return this.current.children;
+  }
+
+  accept(token: Token) {
+  }
+
+  protected insertDoctype(doctypeToken: DoctypeToken) {
+    const documentType = new StaticDocumentType(
+        this.current,
+        this.currentChildNodes.length,
+        doctypeToken.name ?? 'html',
+        doctypeToken.publicId ?? '',
+        doctypeToken.systemId ?? '');
+    this.currentChildNodes.push(documentType);
+  }
+
+  protected insertDataNode(token: TextToken) {
+    const nodeType = TokenTypeMapping[token.type];
+    const dataNode = new StaticDataNode(nodeType, this.current, this.currentChildNodes.length, token.data);
+    this.currentChildNodes.push(dataNode);
+  }
+
+  popUntilMatches(test: (element: StaticElement, name: string) => boolean) {
+    let i = this.openElements.length;
+    if (i) {
+      let changed = false;
+      let element!: StaticElement;
+      for (--i; i >= 0; --i) {
+        element = this.openElements[i];
+        const name = element.tagName;
+        if (test(element, name)) {
+          this.openElements.pop();
+          this.openCounts[name]--;
+          changed = true;
+        } else
+          break;
+      }
+      if (changed)
+        this.current = i < 0 ? this.document : element;
+    }
+  }
+  generateImpliedEndTagsFromSet(closable: { [tagName: string]: any }, exclude?: string) {
+    this.popUntilMatches((element, name) => name !== exclude && (name in closable));
+  }
+  generateImpliedEndTags(exclude?: string) {
+    return this.generateImpliedEndTagsFromSet(IMPLICITLY_CLOSABLE, exclude);
+  }
+  generateImpliedEndTagsThoroughly() {
+    return this.generateImpliedEndTagsFromSet(IMPLICITLY_THOROUGHLY_CLOSABLE);
+  }
+  popCurrentElement() {
+    const element = this.openElements.pop()!;
+    this.openCounts[element.tagName]--;
+    this.current = this.openElements.at(-1) || this.document;
+  }
+
+  forceElementAndState(element: string, state: InsertionMode, token: Token): InsertionMode {
+    this.createAndPushElement({
+      type: 'startTag',
+      name: element,
+      selfClosing: false,
+      attributes: []
+    } as TagToken);
+    return this.reprocessIn(state, token);
+  }
+
+  createAndAddEmptyElement(token: TagToken): StaticElement {
+    const element = new StaticElement(token, this.current, [], this.currentChildNodes.length, this.currentChildElements.length, []);
+    this.currentChildNodes.push(element);
+    this.currentChildElements.push(element);
+    return element;
+  }
+
+  createAndPushElement(token: TagToken): StaticElement {
+    const childNodes: Node[] = [];
+    const childElements: StaticElement[] = [];
+    const element = new StaticElement(token, this.current, childNodes, this.currentChildNodes.length, this.currentChildElements.length, childElements);
+    this.currentChildNodes.push(element);
+    this.currentChildElements.push(element);
+    this.current = element;
+    this.openCounts[token.name] = (this.openCounts[token.name] || 0) + 1;
+    return element;
+  }
+
+  startTextMode(tokenizerState: State, token: TagToken): InsertionMode {
+    this.createAndPushElement(token);
+    this.originalInsertionMode = this.insertionMode;
+    this.tokenizer.state = tokenizerState;
+    this.tokenizer.lastOpenTag = token.name;
+    return 'text';
+  }
+
+  startTemplate(start: TagToken): InsertionMode {
+    // TODO
+    return this.insertionMode;
+  }
+
+  endTemplate(end: TagToken): InsertionMode {
+    // TODO
+    return this.insertionMode;
+  }
+
+  reprocessIn(mode: InsertionMode, token: Token): InsertionMode {
+    this.insertionMode = mode;
+    // @ts-ignore
+    return this[mode](token);
+  }
+  error() {
+    // TODO
+  }
+
+  closeParagraph() {
+    if (this.openCounts['p']) {
+      this.popUntilMatches((element, name) => name !== 'p');
+      if (this.openElements.length) {
+        this.openElements.pop();
+        this.openCounts['p']--;
+        this.current = this.openElements[this.openElements.length - 1] || this.document;
+      }
+    } else {
+      this.openCounts = {};
+      this.openElements.length = 0;
+      this.current = this.document;
+    }
+  }
+}
