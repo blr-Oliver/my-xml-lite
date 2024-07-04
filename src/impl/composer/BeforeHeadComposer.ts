9fc2a04525b1f976dd3e092d028b8cadb4832201
blr-Oliver <vasily.liaskovsky@gmail.com>
2024-07-05 02:19:40 +0300

    separate composer part-classes for starter insertion modes

--- src/impl/composer/BeforeHeadComposer.ts
+++ src/impl/composer/BeforeHeadComposer.ts
@@ -1,100 +1,60 @@
-import {CharactersToken, CommentToken, DoctypeToken, TagToken, Token} from '../tokens';
+import {CharactersToken, CommentToken, TagToken, Token} from '../tokens';
 import {BaseComposer} from './BaseComposer';
 import {InsertionMode} from './insertion-mode';
 
 export class BeforeHeadComposer extends BaseComposer {
-  initial(token: Token): InsertionMode {
-    switch (token.type) {
-      case 'comment':
-        this.insertComment(token as CommentToken);
-        return 'initial';
-      case 'doctype':
-        this.insertDoctype(token as DoctypeToken);
-        // TODO set doctype of current document
-        return 'beforeHtml';
-      default:
-        //whitespace is ignored on tokenizer level
-        this.error('missing-doctype');
-        return this.reprocessIn('beforeHtml', token);
-    }
-  }
-
-  beforeHtml(token: Token): InsertionMode {
-    switch (token.type) {
-      case 'comment':
-        this.insertComment(token as CommentToken);
-        return 'beforeHtml';
-      case 'doctype':
-        this.error();
-        return 'beforeHtml';
-      case 'startTag':
-        const tagToken = token as TagToken;
-        if (tagToken.name === 'html') {
-          this.createAndInsertHTMLElement(tagToken);
-          return 'beforeHead';
-        }
-        return this.forceElementAndState('html', 'beforeHead', token);
-      case 'endTag':
-        return this.beforeHtmlEndTag(token as TagToken);
-      default:
-        return this.forceElementAndState('html', 'beforeHead', token);
-    }
-  }
-
-  beforeHtmlEndTag(token: TagToken): InsertionMode {
-    switch (token.name) {
-      case 'head':
-      case 'body':
-      case 'html':
-      case 'br':
-        return this.forceElementAndState('html', 'beforeHead', token);
-      default:
-        this.error();
-        return 'beforeHtml';
-    }
-  }
-
   beforeHead(token: Token): InsertionMode {
-    let tagToken = token as TagToken;
     switch (token.type) {
       case 'characters':
-        if (!(token as CharactersToken).whitespaceOnly)
-          return this.forceHead(token);
-        break;
+        return this.beforeHeadCharacters(token as CharactersToken);
       case 'comment':
         this.insertComment(token as CommentToken);
         break;
       case 'doctype':
-        this.error();
+        this.error('unexpected-doctype');
         break;
       case 'startTag':
-        switch (tagToken.name) {
-          case 'html':
-            return this.inBody(token);
-          case 'head':
-            this.headElement = this.createAndInsertHTMLElement(tagToken);
-            return 'inHead';
-          default:
-            return this.forceHead(token);
-        }
+        return this.beforeHeadStartTag(token as TagToken);
       case 'endTag':
-        switch (tagToken.name) {
-          case 'head':
-          case 'body':
-          case 'html':
-          case 'br':
-            return this.forceHead(token);
-          default:
-            this.error();
-        }
-        break;
+        return this.beforeHeadEndTag(token as TagToken);
       default:
-        return this.forceHead(token)
+        return this.forceHead(token);
     }
     return this.insertionMode;
   }
 
-  forceHead(token: Token): InsertionMode {
+  beforeHeadCharacters(token: CharactersToken): InsertionMode {
+    if (token.whitespaceOnly)
+      return this.forceHead(token);
+    return this.insertionMode;
+  }
+
+  beforeHeadStartTag(token: TagToken) {
+    switch (token.name) {
+      case 'html':
+        return this.inBody(token);
+      case 'head':
+        this.headElement = this.createAndInsertHTMLElement(token);
+        return 'inHead';
+      default:
+        return this.forceHead(token);
+    }
+  }
+
+  beforeHeadEndTag(tagToken: TagToken): InsertionMode {
+    switch (tagToken.name) {
+      case 'head':
+      case 'body':
+      case 'html':
+      case 'br':
+        return this.forceHead(tagToken);
+      default:
+        this.error('unexpected-tag-before-head');
+    }
+    return this.insertionMode;
+  }
+
+  protected forceHead(token: Token): InsertionMode {
     this.headElement = this.createAndInsertHTMLElement({
       type: 'startTag',
       name: 'head',
--- src/impl/composite-composer.ts
+++ src/impl/composite-composer.ts
@@ -4,12 +4,14 @@ import {AfterBodyComposer} from './composer/AfterBodyComposer';
 import {AfterFramesetComposer} from './composer/AfterFramesetComposer';
 import {BaseComposer} from './composer/BaseComposer';
 import {BeforeHeadComposer} from './composer/BeforeHeadComposer';
+import {BeforeHtmlComposer} from './composer/BeforeHtmlComposer';
 import {HeadComposer} from './composer/HeadComposer';
 import {InBodyComposer} from './composer/InBodyComposer';
 import {InCaptionComposer} from './composer/InCaptionComposer';
 import {InCellComposer} from './composer/InCellComposer';
 import {InColumnGroupComposer} from './composer/InColumnGroupComposer';
 import {InFramesetComposer} from './composer/InFramesetComposer';
+import {InitialComposer} from './composer/InitialComposer';
 import {InRowComposer} from './composer/InRowComposer';
 import {InSelectComposer} from './composer/InSelectComposer';
 import {InSelectInTableComposer} from './composer/InSelectInTableComposer';
@@ -21,6 +23,8 @@ import {TokenAdjustingComposer} from './composer/TokenAdjustingComposer';
 const SyntheticComposerClass = combine('SyntheticComposerClass',
     BaseComposer,
     TokenAdjustingComposer,
+    InitialComposer,
+    BeforeHtmlComposer,
     BeforeHeadComposer,
     HeadComposer,
     InBodyComposer,
@@ -39,6 +43,8 @@ const SyntheticComposerClass = combine('SyntheticComposerClass',
     AfterAfterComposer
 ) as Class<BaseComposer &
     TokenAdjustingComposer &
+    InitialComposer &
+    BeforeHtmlComposer &
     BeforeHeadComposer &
     HeadComposer &
     InBodyComposer &
