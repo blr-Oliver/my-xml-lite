12117eac56e4df86c7b646de6034a511b72ec420
blr-Oliver <vasily.liaskovsky@gmail.com>
2024-08-07 13:58:36 +0300

    added experiments to VCS

---
+++ test/experiment/challenge.ts
@@ -0,0 +1,32 @@
+import * as fs from 'fs';
+import {JSDOM} from 'jsdom';
+import {hrtime} from 'node:process';
+import {parseFromString} from '../../src/impl/parse.js';
+import {SelfClosingOptions, Serializer} from '../../src/impl/Serializer.js';
+
+const projectRoot = 'D:/Dev/Idea/workspace/my-xml-lite/';
+const challengeRoot = projectRoot + 'etc/experiment/';
+const srcPath = 'HTML_Standard.html';
+const litePath = 'out/HTML_Standard_lite.html';
+const jsdomPath = 'out/HTML_Standard_jsdom.html';
+const html: string = fs.readFileSync(challengeRoot + srcPath, {encoding: 'utf-8'});
+
+let now = hrtime.bigint();
+const liteDoc = parseFromString(html);
+const liteTime = hrtime.bigint() - now;
+now = hrtime.bigint();
+const jsdomContext = new JSDOM(html);
+const jsdomTime = hrtime.bigint() - now;
+
+const serializer = new Serializer({
+  htmlVoidSelfClose: SelfClosingOptions.KEEP_SKIP,
+  foreignVoidSelfClose: SelfClosingOptions.KEEP_SKIP,
+  omitEmptyAttributeValue: true,
+  escapeSingleQuoteInAttribute: true,
+  keepCDataSections: false
+});
+
+fs.writeFileSync(challengeRoot + litePath, serializer.serializeNode(liteDoc), {encoding: 'utf-8'});
+fs.writeFileSync(challengeRoot + jsdomPath, jsdomContext.serialize(), {encoding: 'utf-8'});
+
+console.log(`Lite: ${liteTime} (${Number(jsdomTime * 100n / liteTime) / 100}x faster); JSDOM: ${jsdomTime}`);
